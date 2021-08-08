fbd7509d9929c253c3393fea5efe07e1dad1b8b7
Cassiana <lima.cglima@gmail.com>
2021-08-08 20:12:02 -0300

    desafio 03 do curso

---
+++ scripts-python/desafio03.py
@@ -0,0 +1,14 @@
+#DESAFIO 03
+
+"""
+Crie um script Python que leia dois números e tente mostrar a soma entre eles.
+exemplo:
+    Primeiro número: 6
+    Segundo número: 3
+    A soma é 9
+"""
+numero1 = int(input('Entre com o primeiro número: '))
+numero2 = int(input('Entre com o segundo número: '))
+soma = numero1 + numero2
+
+print('A soma é', soma)
