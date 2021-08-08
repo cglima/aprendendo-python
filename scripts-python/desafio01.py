de258528d5bb414e86cd202557c43f255472eaea
Cassiana <lima.cglima@gmail.com>
2021-08-08 19:54:43 -0300

    desafio 01 do curso

---
+++ scripts-python/desafio01.py
@@ -0,0 +1,10 @@
+# DESAFIO 01
+
+"""  Criar um script Python que leia o nome de uma pessoa e mostra uma mensagem de
+boas vindas de acordo com o valor digitado.
+exemplo:
+    Qual seu nome?
+    Olá Jupiter ! Prazer em te conhecer! """
+
+nome = input('Qual o seu nome? ')
+print('Olá', nome + '!', 'Prazer em te conhecer!')
