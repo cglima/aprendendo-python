16286e88d9ecfaeae5e491ce56a943e622b2970b
Cassiana <lima.cglima@gmail.com>
2021-08-09 00:36:51 -0300

    desafio 04 do mundo 1

---
+++ mundo-01/desafio04.py
@@ -0,0 +1,21 @@
+#DESAFIO 04
+
+"""
+Faça um programa que leia algo pelo teclado e mostre na tela o seu tipo primitivo
+e todas as informações possíveis sobre ele.
+"""
+
+#n = input('Digite algo: ')
+#print(n.isnumeric())
+
+#algo = input('Digite algo: ')
+#print(algo.isalpha())
+
+#x = input('Digite algo: ')
+#print(x.isdecimal())
+
+#y = input('Digite algo:')
+#print(y.isdigit())
+
+a = input('Digite algo: ')
+print(a.isprintable())
