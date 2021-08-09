de636a4b26baf1eeadc9fa5021e67a66c52b0bcf
Cassiana <lima.cglima@gmail.com>
2021-08-08 22:34:25 -0300

    reorganizei o diretório e renomiei alguns scripts para seguir um padrão de organização

--- mundo-01/ex001.py
+++ mundo-01/ex001.py
@@ -1,3 +1,6 @@
+"""
+EXERCÍCIO 001 - DEIXANDO TUDO PRONTO
+"""
 print('Olá, MUndo!')
 
 msg = 'Olá, Mundo!'
---
+++ mundo-01/ex002.py
@@ -0,0 +1,7 @@
+"""
+EXERCÍCIO 002 - RESPONDENDO AO USUÁRIO
+Faça um programa que leia o nome de uma pessoa e mostre uma mensagem
+de boas-vindas
+"""
+nome = input('Qual o seu nome? ')
+print('Olá {}! É um prazer te conhecer, seja muito bem vindo(a)'.format(nome))
