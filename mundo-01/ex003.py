0321ad384d07b8ca1642bb895a1eeeff8000948b
Cassiana <lima.cglima@gmail.com>
2021-08-09 12:51:41 -0300

    exercicio 3 do mundo 1 - somando números

--- mundo-01/ex003.py
+++ mundo-01/ex003.py
@@ -1,4 +1,11 @@
 """
 EXERCÍCIO 003 - SOMANDO DOIS NÚMEROS
-
+Crie um programa que leia dois números e mostre a soma entre eles.
 """
+
+numero1 = int(input('Digite um número: '))
+numero2 = int(input('Digite outro número: '))
+
+soma = numero1 + numero2
+
+print('A soma do número {0} e do número {1} é igual {2}'.format(numero1, numero2, soma))
