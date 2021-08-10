da5acd238e9aef29eec482dd8c59ee000fee87ee
Cassiana <lima.cglima@gmail.com>
2021-08-09 22:14:49 -0300

    outra forma de resolver o exercicio 5

--- mundo-01/ex005.py
+++ mundo-01/ex005.py
@@ -7,5 +7,11 @@ numero = int(input('Digite um número: '))
 antecessor = numero - 1
 sucessor = numero + 1
 print('\n O número digitado foi o {} \n O seu sucessor é o {} \n'
-.format(numero, sucessor, antecessor), end= ' ')
+.format(numero, sucessor), end= ' ')
 print('O seu antecessor é o {} \n' .format(antecessor))
+
+#OUTRO METODO
+print('\n Outro método de resolução!!! \n')
+
+n = int(input('Digite um número: '))
+print('O numero escolhido é {},\n seu antecessor é {}  \ne o sucessor é {}'.format(n, (n-1), (n+1)))
