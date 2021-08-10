33514f5981ae214f6452ea5b1796929335d39bc9
Cassiana <lima.cglima@gmail.com>
2021-08-10 19:20:14 -0300

    exercicio 9 do mundo 1

---
+++ mundo-01/ex009.py
@@ -0,0 +1,22 @@
+"""
+EXERCÍCIO 009 - TABUADA
+Faça um programa que leia um número inteiro qualquer e mostre na tela a sua tabuada.
+"""
+
+n = int(input('\n Digite um número para ver sua tabuada: '))
+print('-' * 5, 'TABUADA DO {}'.format(n), '-' * 5)
+print('{:7} x {:2} = {}' .format(n, 1, n*1))
+print('{:7} x {:2} = {}' .format(n, 2, n*2))
+print('{:7} x {:2} = {}' .format(n, 3, n*3))
+print('{:7} x {:2} = {}' .format(n, 4, n*4))
+print('{:7} x {:2} = {}' .format(n, 5, n*5))
+print('{:7} x {:2} = {}' .format(n, 6, n*6))
+print('{:7} x {:2} = {}' .format(n, 7, n*7))
+print('{:7} x {:2} = {}' .format(n, 8, n*8))
+print('{:7} x {:2} = {}' .format(n, 8, n*8))
+print('{:7} x {:2} = {}' .format(n, 9, n*9))
+print('{:7} x {:2} = {}' .format(n, 10, n*10))
+print('-' * 23)
+
+
+
