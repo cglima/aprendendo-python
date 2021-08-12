3cb240cb1b57804c4070767b1a21628b4ab501e2
Cassiana <lima.cglima@gmail.com>
2021-08-11 21:43:04 -0300

    exercicio 16 mundo 1

---
+++ mundo-01/ex016.py
@@ -0,0 +1,10 @@
+"""EXERCÍCIO 016 - QUEBRANDO UM NÚMERO
+Crie um programa que leia um número real qualquer pelo teclado e mostre na tela
+a sua porção inteira.
+"""
+'''from math import trunc
+num = float(input('Digite um número: '))
+print('A porção inteira do número {} é {}'.format(num, trunc(num)))'''
+
+num = float(input('Digite um número: '))
+print('A porção inteira do numero {} é {}'.format(num,int(num)))
