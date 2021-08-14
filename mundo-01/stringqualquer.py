529b2328ed461804db9f4386e5929d9491d7ef0a
Cassiana <lima.cglima@gmail.com>
2021-08-14 11:36:15 -0300

    notas sobre uma string qualquer

---
+++ mundo-01/stringqualquer.py
@@ -0,0 +1,48 @@
+# Exemplos com string qualquer
+
+string = 'computador'
+
+#verifica se está em letra maiúscula
+print(string.isupper())
+
+#verifica se está em letra minúscula
+print(string.islower())
+
+#converte para letra maiúscula
+print(string.upper())
+
+# retorna primeira letra
+print(string[0])
+
+#retorna da segunda até a quarta letra
+print(string[1:4])
+
+#retorna a última letra
+print(string[-1])
+
+#retorna a primeira letra
+print(string[0])
+
+#retorna a terceira letra
+print(string[2])
+
+#cria uma string
+var1 = 'AABB'
+
+#cria outra string
+var2 = 'CCDD'
+
+#une as duas strings
+print(var1+var2)
+
+#cria outro exemplo de texto
+exemplo = 'Hello World'
+
+#duplica o texto
+print(exemplo*2)
+
+#verifica se o texto contem alguma palavra específica
+print('World' in exemplo)
+
+#verifica se o texto NÃO contém alguma palavra específica
+print('World' not in exemplo)
