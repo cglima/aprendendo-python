b8891df17edeec16fd7e1f8081e5962f07dadd42
Cassiana <lima.cglima@gmail.com>
2021-08-25 13:46:39 -0300

    anotações sobre manipulando texto na pratica

---
+++ mundo-01/manipulandotexto.py
@@ -0,0 +1,17 @@
+frase = 'Curso em Video Python'
+print(frase)
+print(frase[3])
+
+print(frase[3:13])
+print(frase[:13])
+print(frase[:13])
+print(frase[1:15:2])
+print(frase[::2])
+
+print(frase.count('o'))
+
+print(frase.upper().count('O'))
+print(len(frase))
+
+print("""PARA PRINTAR UM TEXTO ENORME EM PYTHON SEM SE PREOCUPAR EM COLOCAR VÁRIOS PRINTS A CADA FRASE!""")
+''
